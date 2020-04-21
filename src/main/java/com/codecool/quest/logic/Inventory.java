c958cc33512957b8d1e2843de525660d0f7c6330
Steven Trautmann <steven.trautmann1@gmail.com>
2020-04-21 16:10:57 +0200

    creating inventory

---
+++ src/main/java/com/codecool/quest/logic/Inventory.java
@@ -0,0 +1,13 @@
+package com.codecool.quest.logic;
+
+import java.util.ArrayList;
+
+public class Inventory {
+    
+    ArrayList<String> inventory = new ArrayList<>();
+
+    public ArrayList<String> getInventory(){
+        return inventory;
+    }
+
+}
