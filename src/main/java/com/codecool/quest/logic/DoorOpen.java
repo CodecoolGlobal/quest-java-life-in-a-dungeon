c275a5c443f230e8291f02fdbb32476c76052910
aronpachl <pachl.aron@gmail.com>
2020-04-21 15:27:24 +0200

    new file added, check door opened method

---
+++ src/main/java/com/codecool/quest/logic/DoorOpen.java
@@ -0,0 +1,19 @@
+package com.codecool.quest.logic;
+
+import com.codecool.quest.logic.actors.Player;
+import java.util.ArrayList;
+
+public class DoorOpen {
+
+    public static void checkDoors(ArrayList aList, Player player) {
+        Cell cell = player.getCell();
+        Cell[] arrayCell = {cell.getNeighbor(0, -1), cell.getNeighbor(0, 1), cell.getNeighbor(-1, 0), cell.getNeighbor(1, 0)};
+        for (Cell element : arrayCell) {
+            if (element.getDoor() != null && aList.contains("key")) {
+                element.getDoor().setHasKey(true);
+                element.setType(CellType.OPENDOOR);
+            }
+        }
+    }
+}
+
