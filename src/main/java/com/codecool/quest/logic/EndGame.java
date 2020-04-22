28e89b8bc05e39db84be88d2f2fe1658ba594201
Viktor Bocz <beviktor95@gmail.com>
2020-04-22 15:19:42 +0200

    End game endgame level

--- src/main/java/com/codecool/quest/logic/EndGame.java
+++ src/main/java/com/codecool/quest/logic/EndGame.java
@@ -1,4 +1,41 @@
 package com.codecool.quest.logic;
 
+import javafx.geometry.Pos;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
+
+
 public class EndGame {
-}
+
+
+        public static void display(String text) {
+            Stage popUpWindow = new Stage();
+
+            popUpWindow.initModality(Modality.APPLICATION_MODAL);
+            popUpWindow.setTitle("Codecool Quest");
+
+            Label label1= new Label(text + " Another game?");
+
+            Button button1 = new Button("Yes");
+            Button button2 = new Button("No");
+            button1.setOnAction(e -> MapLoader.loadMap());
+            button2.setOnAction(e -> System.exit(0));
+
+            VBox layout= new VBox(10);
+
+
+            layout.getChildren().addAll(label1, button1, button2);
+
+            layout.setAlignment(Pos.CENTER);
+
+            Scene scene1= new Scene(layout, 300, 250);
+
+            popUpWindow.setScene(scene1);
+
+            popUpWindow.showAndWait();
+
+        }
+    }
+
