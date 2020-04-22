46cce4a8f913e7abd937a613386aae36761dc34f
aronpachl <pachl.aron@gmail.com>
2020-04-22 10:39:38 +0200

    method for different fight types

--- src/main/java/com/codecool/quest/logic/Fight.java
+++ src/main/java/com/codecool/quest/logic/Fight.java
@@ -1,17 +1,33 @@
 package com.codecool.quest.logic;
 
 import com.codecool.quest.logic.actors.Player;
+import java.util.ArrayList;
 
 public class Fight {
-    
-    public void standardFight(Player player){
+
+    // We have to check the names of inventory items.
+    public void checkWhichKindOfFightYouFightWithEnemyIfYouHaveItemsInYourInventory(Player player){
+        Cell[] arrayCell = Cell.returnNeighbors(player.getCell());
+        ArrayList<String> inventory = player.getStuffedInventory();
+        if(inventory.contains("sword") && !inventory.contains("shield")){
+            specialFight(player, arrayCell, 2, 10);
+        } else if(inventory.contains("sword") && inventory.contains("shield")){
+            specialFight(player, arrayCell, 0, 10);
+        } else if(!inventory.contains("sword") && inventory.contains("shield")){
+            specialFight(player, arrayCell, 0, 5);
+        } else {
+            specialFight(player, arrayCell, 2, 5);
+        }
+    }
+
+    public void specialFight(Player player, Cell[] arrayCell, int playerDamage, int enemyDamage){
         HealthLogic healthlogic = new HealthLogic();
-        Cell cell = player.getCell();
-        Cell[] arrayCell = {cell.getNeighbor(0, -1), cell.getNeighbor(0, 1), cell.getNeighbor(-1, 0), cell.getNeighbor(1, 0)};
         for(Cell element: arrayCell){
             if(element.getActor() != null && element.getActor().getHealth() > 0 && player.getHealth() > 0){
-                healthlogic.decreaseLife(player, 2, element.getActor(), 5);
+                healthlogic.decreaseLife(player, playerDamage, element.getActor(), enemyDamage);
             }
         }
     }
+
+
 }
