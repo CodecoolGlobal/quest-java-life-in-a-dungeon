a3d7bb51916bc483bb2a2c09bdcd858d1af98e04
Viktor Bocz <beviktor95@gmail.com>
2020-04-22 15:19:18 +0200

    End game DoorOpen level

--- src/main/java/com/codecool/quest/logic/DoorOpen.java
+++ src/main/java/com/codecool/quest/logic/DoorOpen.java
@@ -7,6 +7,9 @@ public class DoorOpen {
 
     public static void checkDoors(ArrayList aList, Player player) {
         Cell cell = player.getCell();
+        if(cell.getTileName().equals("exitDoor")) {
+            EndGame.display("Congratulations!");
+        }
         Cell[] arrayCell = {cell.getNeighbor(0, -1), cell.getNeighbor(0, 1), cell.getNeighbor(-1, 0), cell.getNeighbor(1, 0)};
         for (Cell element : arrayCell) {
             if (element.getDoor() != null && aList.contains("key")) {
@@ -15,5 +18,6 @@ public class DoorOpen {
             }
         }
     }
+
 }
 
