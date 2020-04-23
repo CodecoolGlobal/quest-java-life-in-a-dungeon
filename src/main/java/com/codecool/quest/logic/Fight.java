9e1e570cbd9d1dc0d86ccffec11f5c73de800520
Steven Trautmann <steven.trautmann1@gmail.com>
2020-04-23 13:58:23 +0200

    add special fight with shield

--- src/main/java/com/codecool/quest/logic/Fight.java
+++ src/main/java/com/codecool/quest/logic/Fight.java
@@ -1,32 +1,41 @@
 package com.codecool.quest.logic;
 
 import com.codecool.quest.logic.actors.Player;
+import javafx.scene.layout.GridPane;
+import com.codecool.quest.logic.RemoveNode;
+
 import java.util.ArrayList;
 
 public class Fight {
 
     // We have to check the names of inventory items.
-    public void checkWhichKindOfFightYouFightWithEnemyIfYouHaveItemsInYourInventory(Player player){
+    public boolean checkWhichKindOfFightYouFightWithEnemyIfYouHaveItemsInYourInventory(Player player) {
         Cell[] arrayCell = Cell.returnNeighbors(player.getCell());
         ArrayList<String> inventory = player.getStuffedInventory();
-        if(inventory.contains("sword") && !inventory.contains("shield")){
-            specialFight(player, arrayCell, 2, 10);
-        } else if(inventory.contains("sword") && inventory.contains("shield")){
-            specialFight(player, arrayCell, 0, 10);
-        } else if(!inventory.contains("sword") && inventory.contains("shield")){
-            specialFight(player, arrayCell, 0, 5);
-        } else {
-            specialFight(player, arrayCell, 2, 5);
-        }
-    }
-
-    public void specialFight(Player player, Cell[] arrayCell, int playerDamage, int enemyDamage){
-        for(Cell element: arrayCell){
-            if(element.getActor() != null && element.getActor().getHealth() > 0 && player.getHealth() > 0){
-                HealthLogic.decreaseLife(player, playerDamage, element.getActor(), enemyDamage);
+        for (Cell element : arrayCell) {
+            if (element.getActor() != null && element.getActor().getHealth() > 0 && player.getHealth() > 0) {
+                if (inventory.contains("sword") && !inventory.contains("shield")) {
+                    specialFight(player, element, 2, 10);
+                } else if (inventory.contains("sword") && inventory.contains("shield")) {
+                    specialFightWithShield(player, element, 10, inventory);
+                    return true;
+                } else if (!inventory.contains("sword") && inventory.contains("shield")) {
+                    specialFightWithShield(player, element, 5, inventory);
+                    return true;
+                } else {
+                    specialFight(player, element, 2, 5);
+                }
             }
         }
+        return false;
     }
 
+    public void specialFight(Player player, Cell cell, int playerDamage, int enemyDamage) {
+        HealthLogic.decreaseLife(player, playerDamage, cell.getActor(), enemyDamage);
+    }
 
+    public void specialFightWithShield(Player player, Cell cell, int enemyDamage, ArrayList<String> inv) {
+            HealthLogic.decreaseLife(player, 0, cell.getActor(), enemyDamage);
+            inv.remove("shield");
+    }
 }
