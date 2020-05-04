c3962f5c9e4e07552f8ae646fceec38fe3b534be
Viktor Bocz <beviktor95@gmail.com>
2020-05-04 16:46:26 +0200

    Big levels feature dooropen

--- src/main/java/com/codecool/quest/logic/DoorOpen.java
+++ src/main/java/com/codecool/quest/logic/DoorOpen.java
@@ -1,6 +1,7 @@
 package com.codecool.quest.logic;
 
 import com.codecool.quest.logic.actors.Player;
+import com.codecool.quest.logic.items.Item;
 import javafx.collections.ObservableList;
 import javafx.scene.Node;
 import javafx.scene.image.ImageView;
@@ -32,23 +33,28 @@ public class DoorOpen {
 //    }
 
 
-    public static boolean checkDoors(ArrayList aList, Player player, GridPane ui, int keyIndex) {
+    public static boolean checkDoors(ArrayList<Item> ItemList, Player player, GridPane ui, int keyIndex) {
         Cell cell = player.getCell();
-        if(cell.getTileName().equals("exitDoor")) {
+        ArrayList<String> aList = new ArrayList<>();
+        for (Item items : ItemList) {
+            aList.add(items.getTileName());
+        }
+        if (cell.getTileName().equals("exitDoor")) {
             EndGame.display("Congratulations!");
         }
         Cell[] arrayCell = {cell.getNeighbor(0, -1), cell.getNeighbor(0, 1), cell.getNeighbor(-1, 0), cell.getNeighbor(1, 0)};
         for (Cell element : arrayCell) {
             if (element.getTileName().equals("closedDoor") && aList.contains("key")) {
+                int indexForKey = aList.indexOf("key");
                 element.getDoor().setHasKey(true);
-                aList.remove("key");
-                RemoveNode.removeNodeByRowColumnIndex(5, keyIndex-1,ui);
+                ItemList.remove(indexForKey);
+                RemoveNode.removeNodeByRowColumnIndex(5, keyIndex - 1, ui);
                 element.setType(CellType.OPENDOOR);
                 return true;
             }
         }
         return false;
     }
-
 }
+
 
