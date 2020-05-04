e10288d2097a7b080082ee80a73f0c51ef6a3e35
Viktor Bocz <beviktor95@gmail.com>
2020-05-04 16:47:03 +0200

    Big levels feature Fight part

--- src/main/java/com/codecool/quest/logic/Fight.java
+++ src/main/java/com/codecool/quest/logic/Fight.java
@@ -1,6 +1,7 @@
 package com.codecool.quest.logic;
 
 import com.codecool.quest.logic.actors.Player;
+import com.codecool.quest.logic.items.Item;
 import javafx.scene.layout.GridPane;
 import com.codecool.quest.logic.RemoveNode;
 
@@ -8,34 +9,52 @@ import java.util.ArrayList;
 
 public class Fight {
 
+
     // We have to check the names of inventory items.
-    public boolean checkWhichKindOfFightYouFightWithEnemyIfYouHaveItemsInYourInventory(Player player) {
+    public Boolean checkWhichKindOfFightYouFightWithEnemyIfYouHaveItemsInYourInventory(Player player) {
         Cell[] arrayCell = Cell.returnNeighbors(player.getCell());
-        ArrayList<String> inventory = player.getStuffedInventory();
-        for (Cell element : arrayCell) {
-            if (element.getActor() != null && element.getActor().getHealth() > 0 && player.getHealth() > 0) {
-                if (inventory.contains("sword") && !inventory.contains("shield")) {
-                    specialFight(player, element, 3, 10);
-                } else if (inventory.contains("sword") && inventory.contains("shield")) {
-                    specialFightWithShield(player, element, 10, inventory);
-                    return true;
-                } else if (!inventory.contains("sword") && inventory.contains("shield")) {
-                    specialFightWithShield(player, element, 5, inventory);
-                    return true;
-                } else {
-                    specialFight(player, element, 3, 5);
-                }
-            }
+        ArrayList<Item> inventory = player.getStuffedInventory();
+        ArrayList<String> inventory1 = new ArrayList<>();
+
+        for (int i = 0; i < inventory.size(); i++) {
+            inventory1.add(inventory.get(i).getTileName());
+        }
+        if (inventory1.contains("sword") && !inventory1.contains("shield")) {
+            specialFight(player, arrayCell, 2, 10);
+        } else if (inventory1.contains("sword") && inventory1.contains("shield")) {
+            int index = inventory1.indexOf("shield");
+            specialFightWithShield(player, arrayCell, 10, inventory, index);
+            return true;
+        } else if (!inventory1.contains("sword") && inventory1.contains("shield")) {
+            int index = inventory1.indexOf("shield");
+            specialFightWithShield(player, arrayCell, 5,inventory, index);
+            return true;
+        } else {
+            specialFight(player, arrayCell, 2, 5);
         }
         return false;
     }
 
-    public void specialFight(Player player, Cell cell, int playerDamage, int enemyDamage) {
-        HealthLogic.decreaseLife(player, playerDamage, cell.getActor(), enemyDamage);
+
+    public void specialFight(Player player, Cell[] arrayCell, int playerDamage, int enemyDamage) {
+        for (Cell element : arrayCell) {
+            if (element.getActor() != null && element.getActor().getHealth() > 0 && player.getHealth() > 0) {
+                HealthLogic.decreaseLife(player, playerDamage, element.getActor(), enemyDamage);
+            }
+        }
     }
 
-    public void specialFightWithShield(Player player, Cell cell, int enemyDamage, ArrayList<String> inv) {
-            HealthLogic.decreaseLife(player, 0, cell.getActor(), enemyDamage);
-            inv.remove("shield");
+    public void specialFightWithShield(Player player, Cell[] arrayCell, int enemyDamage, ArrayList<Item> inv, int ind) {
+        for (Cell element : arrayCell) {
+            if (element.getActor() != null && element.getActor().getHealth() > 0 && player.getHealth() > 0) {
+                HealthLogic.decreaseLife(player, 0, element.getActor(), enemyDamage);
+                inv.get(ind).setHealth(inv.get(ind).getHealth()-1);
+                if (inv.get(ind).getHealth() == 0) {
+                    inv.remove(inv.get(ind));
+                }
+            }
+        }
     }
+
+
 }
