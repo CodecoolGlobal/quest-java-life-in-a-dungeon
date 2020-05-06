54744ea465418268273c41adc99a6fc3505e2a36
Viktor Bocz <beviktor95@gmail.com>
2020-05-06 16:44:05 +0200

    Collect skull Inventory level

--- src/main/java/com/codecool/quest/logic/Inventory.java
+++ src/main/java/com/codecool/quest/logic/Inventory.java
@@ -1,5 +1,6 @@
 package com.codecool.quest.logic;
 
+import com.codecool.quest.logic.actors.Actor;
 import com.codecool.quest.logic.items.Item;
 
 import java.util.ArrayList;
@@ -7,10 +8,13 @@ import java.util.ArrayList;
 public class Inventory {
     
     ArrayList<Item> inventory = new ArrayList<>();
+    ArrayList<Actor> skullInventory = new ArrayList<>();
 
     public ArrayList<Item> getInventory(){
         return inventory;
     }
 
-
+    public ArrayList<Actor> getSkullInventory() {
+        return skullInventory;
+    }
 }
