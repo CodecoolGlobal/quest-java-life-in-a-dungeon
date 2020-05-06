d16277b92618e32d844396ddd6979b6e438947c6
aronpachl <pachl.aron@gmail.com>
2020-05-06 17:38:31 +0200

    random move for skeletons

--- src/main/java/com/codecool/quest/logic/actors/Skeleton.java
+++ src/main/java/com/codecool/quest/logic/actors/Skeleton.java
@@ -3,9 +3,20 @@ package com.codecool.quest.logic.actors;
 import com.codecool.quest.logic.Cell;
 import com.codecool.quest.logic.actors.Actor;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
 public class Skeleton extends Actor {
+    private Cell cell;
+
+
     public Skeleton(Cell cell) {
         super(cell);
+        this.cell = cell;
+        setAttackDamage(2);
     }
 
     @Override
@@ -14,5 +25,16 @@ public class Skeleton extends Actor {
             return "deathSkeleton";
         }
         return "skeleton";
+    }
+
+    @Override
+    public void move(int dx, int dy) {
+        Cell nextCell = cell.getNeighbor(dx, dy);
+        if (this.getTileName().equals("skeleton") && !nextCell.getTileName().matches("wall|empty|closedDoor|spiderWeb") &&
+                (nextCell.getActor() == null || !nextCell.getActor().getTileName().matches("skeleton|player|golem|deathGolem|deathSkeleton|spider"))){
+            cell.setActor(null);
+            nextCell.setActor(this);
+            cell = nextCell;
+        }
     }
 }
