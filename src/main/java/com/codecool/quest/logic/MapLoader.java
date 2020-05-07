2a1ba7e6b3db953d383001508fea995cebc88f52
aronpachl <pachl.aron@gmail.com>
2020-05-07 19:24:05 +0200

    different letters added

--- src/main/java/com/codecool/quest/logic/MapLoader.java
+++ src/main/java/com/codecool/quest/logic/MapLoader.java
@@ -88,6 +88,46 @@ public class MapLoader {
                         case 'g':
                             cell.setType(CellType.GRASS);
                             break;
+                        case 'x':
+                            cell.setType(CellType.FLOOR);
+                            new Letter(cell, "golemLetter", "These are the Golems: \n" +
+                                    "Golems can't move but has a big attack damage(4)");
+                            break;
+                        case 'y':
+                            cell.setType(CellType.FLOOR);
+                            new Letter(cell, "skeletonLetter", "These are the Skeletons: \n" +
+                                    "They run randomly, fast and have a medium attack damage(2)");
+                            break;
+                        case 'z':
+                            cell.setType(CellType.FLOOR);
+                            new Letter(cell, "itemsLetter", "These are the items that you can use during your adventure \n" +
+                                    "Sword: increase your attack damage to 10 \n" +
+                                    "Key: you need to pick up a key to be able to open a closed door. \n" +
+                                    "Closed door: a closed door which can be opened with a key. \n" +
+                                    "Shield: shield has 4 health, so it can helpful against monsters. \n" + "After it's lost it health you will lose your shield!");
+                            break;
+                        case 'q':
+                            cell.setType(CellType.FLOOR);
+                            new Letter(cell, "spiderLetter", "These are the Spiders: \n" +
+                                    "They move randomly, but slowly and have a quite low attack damage(1). \n" +
+                                    "Spiders are peaceful and won't attack you unless you hit them first. \n" +
+                                    "They also make webs whenever they move.");
+                            break;
+                        case 'n':
+                            cell.setType(CellType.FLOOR);
+                            new Letter(cell, "exitLetter", "This is the end of each map: \n" +
+                                    "(HINT: Skulls might also increase your health.)");
+                            break;
+                        case '1':
+                            cell.setType(CellType.FLOOR);
+                            new Letter(cell, "firstMapMission", "You need to collect 3 skulls to accomplish this map! \n" +
+                                    "Good luck!");
+                            break;
+                        case '2':
+                            cell.setType(CellType.FLOOR);
+                            new Letter(cell, "secondMapMission", "You need to collect 10 skulls to accomplish this map! \n" +
+                                    "Good luck!");
+                            break;
                         default:
                             throw new RuntimeException("Unrecognized character: '" + line.charAt(x) + "'");
                     }
